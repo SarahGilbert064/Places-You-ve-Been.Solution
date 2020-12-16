bcdd3551dd286d6c0a19fe491fcaa3644b9c1ca6
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-12-16 11:27:59 -0800

    correctly pass GetAll test
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

--- PlacesYouveBeen.Tests/ModelTests/PlaceTests.cs
+++ PlacesYouveBeen.Tests/ModelTests/PlaceTests.cs
@@ -19,6 +19,17 @@ namespace PlacesYouveBeen.Tests
       Place newPlace = new Place("test");
       Assert.AreEqual(typeof(Place), newPlace.GetType());
     }
-    
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_PlaceList()
+    {
+      List<Place> newEmptyList = new List<Place> {};
+      // /*creates a new place "test" and automatically adds it to _instances list*/ " Place newPlace = new Place("test"); "
+      //code on line 26 will correctly fail test we are putting 'test' into a list that should be returning empty 'at this time'
+      /*returns _instaces list and saves as result*/ 
+      List<Place> result = Place.GetAll();
+      CollectionAssert.AreEqual(newEmptyList, result);
+    }
+
+
   }
 }
